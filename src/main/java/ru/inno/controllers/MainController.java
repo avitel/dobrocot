129f4f8e26b46aca8ccb91d7785bc0f954f4b397
MironKim <mithoughts@yandex.ru>
2019-02-20 16:55:32 +0300

    Главная страница сервиса через контроллер. 24.

---
+++ src/main/java/ru/inno/controllers/MainController.java
@@ -0,0 +1,42 @@
+package ru.inno.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.inno.services.SearchService;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    private SearchService searchService = new SearchService();
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String mainPageGET(Model springModel) {
+
+        springModel.addAttribute("filterOption", searchService.getFilterOption());
+        springModel.addAttribute("result", searchService.getFilteredCars(null, null, null, null));
+
+        return "mainpage";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String mainPagePOST(@RequestParam(name = "mark", required = false) String makr,
+                               @RequestParam(name = "model", required = false) String model,
+                               @RequestParam(name = "engine", required = false) String engine,
+                               @RequestParam(name = "color", required = false) String color,
+                               Model springModel) {
+        springModel.addAttribute("filterOption", searchService.getFilterOption());
+        springModel.addAttribute("result", searchService.getFilteredCars(
+                null == makr ? null : Integer.valueOf(makr),
+                null == model ? null : Integer.valueOf(model),
+                null == engine ? null : Integer.valueOf(engine),
+                null == color ? null : Integer.valueOf(color)
+        ));
+        return "mainpage";
+    }
+}
---
+++ src/main/webapp/pages/mainpage.jsp
@@ -0,0 +1,29 @@
+<%@ page import="java.util.Collection" %>
+<%@ page import="java.util.List" %>
+<%@ page import="ru.inno.entity.*" %>
+<%@ page import="java.util.Map" %>
+<%@ page import="java.util.Set" %><%--
+  Created by IntelliJ IDEA.
+  User: ilya
+  Date: 2019-02-12
+  Time: 17:07
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Dobrocot</title>
+</head>
+<body>
+Main page from controller
+<br/>
+filterOption
+${filterOption}
+<br/>
+<br/>
+result
+<br/>
+${result}
+</body>
+</html>
+

b1deaaea80d894ed43cd55a2eb5c4a56cc80e707
MironKim <mithoughts@yandex.ru>
2019-02-22 16:46:23 +0300

    Добавлена проверка авторизованности пользователя. Ошибки перенаправляются на /error с параметром "error" и сообщением об ошибке.

--- src/main/java/ru/inno/controller/MainController.java
+++ src/main/java/ru/inno/controller/MainController.java
@@ -1,18 +1,27 @@
 package ru.inno.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import ru.inno.ConnectionManager;
+import ru.inno.Security;
+import ru.inno.dao.PersonImpl;
 import ru.inno.services.SearchService;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
     private SearchService searchService;
-
 
     public MainController(SearchService searchService) {
         this.searchService = searchService;
@@ -20,18 +29,22 @@ public class MainController {
 
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView mainPageGET(ModelAndView modelAndView) {
+        if (!addUser(modelAndView)) return modelAndView;
         modelAndView.addObject("filterOption", searchService.getFilterOption());
-        modelAndView.addObject("result", searchService.getFilteredCars(null, null, null, null));
-        modelAndView.setViewName("mainpage");
-        return modelAndView;
+            modelAndView.addObject("result", searchService.getFilteredCars(null, null, null, null));
+            modelAndView.setViewName("mainpage");
+            return modelAndView;
+
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public ModelAndView mainPagePOST(@RequestParam(name = "mark", required = false) String makr,
-                               @RequestParam(name = "model", required = false) String model,
-                               @RequestParam(name = "engine", required = false) String engine,
-                               @RequestParam(name = "color", required = false) String color,
-                               ModelAndView modelAndView) {
+                                     @RequestParam(name = "model", required = false) String model,
+                                     @RequestParam(name = "engine", required = false) String engine,
+                                     @RequestParam(name = "color", required = false) String color,
+                                     ModelAndView modelAndView) {
+
+        if (!addUser(modelAndView)) return modelAndView;
         modelAndView.addObject("filterOption", searchService.getFilterOption());
         modelAndView.addObject("result", searchService.getFilteredCars(
                 null == makr ? null : Integer.valueOf(makr),
@@ -42,4 +55,19 @@ public class MainController {
         modelAndView.setViewName("mainpage");
         return modelAndView;
     }
+
+    private boolean addUser(ModelAndView modelAndView) {
+        try (Connection connection = ConnectionManager.getConnection()) {
+            Security security = new Security(SecurityContextHolder.getContext(), new PersonImpl(connection));
+            modelAndView.addObject("user", null == security.getCurrentUser() ? null : security.getCurrentUser().getName());
+        } catch (SQLException e) {
+            LOGGER.error("MainController.mainPage();", e);
+            modelAndView.addObject("error", "Произошла непредвиденная ошибка :( ");
+            modelAndView.setViewName("error");
+            return false;
+        }
+        return true;
+    }
+
+
 }

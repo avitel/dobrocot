c4b5eed764b5f6e7c4b6659203453057218fae64
stritron <finalroot@gmail.com>
2019-02-22 17:24:31 +0300

    Added getCarList method

--- src/main/java/ru/inno/services/CabinetService.java
+++ src/main/java/ru/inno/services/CabinetService.java
@@ -1,16 +1,29 @@
 package ru.inno.services;
 
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import ru.inno.ConnectionManager;
+import ru.inno.Security;
+import ru.inno.dao.*;
 import ru.inno.entity.Car;
 import ru.inno.entity.Order;
+import ru.inno.entity.Person;
 
+import java.sql.Connection;
 import java.util.List;
 
 @Service
 public class CabinetService {
 
     public List<Car> getCarList(){
+        Security security = new Security(SecurityContextHolder.getContext(), new PersonImpl(ConnectionManager.getConnection()));
+        Person user = security.getCurrentUser();
+        Connection c = ConnectionManager.getConnection();
+        CarDAO carDAO = new CarImpl(c);
+        List<Car> carList = carDAO.getCarsByPerson(user.getId());
 
+        ConnectionManager.closeConnection(c);
         return null;
     }
 

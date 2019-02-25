f2ca2997c9c94a68e2a71633a625857ee60e7094
MironKim <mithoughts@yandex.ru>
2019-02-25 20:27:11 +0300

    Класс конфигурации бинов

---
+++ src/main/java/ru/inno/config/DobrocotConfig.java
@@ -0,0 +1,49 @@
+package ru.inno.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.inno.ConnectionManager;
+import ru.inno.dao.*;
+
+import java.sql.Connection;
+
+@Configuration
+public class DobrocotConfig {
+
+    @Bean
+    Connection connection() {
+        return ConnectionManager.getConnection();
+    }
+
+    @Bean
+    CarDAO carDAO(Connection connection) {
+        return new CarImpl(connection);
+    }
+
+    @Bean
+    MarkDAO markDAO(Connection connection){
+        return new MarkImpl(connection);
+    }
+
+    @Bean
+    ModelDAO modelDAO(Connection connection) {
+        return new ModelImpl(connection);
+    }
+
+    @Bean
+    ColorDAO colorDAO(Connection connection) {
+        return new ColorImpl(connection);
+    }
+
+    @Bean
+    EngineDAO engineDAO(Connection connection) {
+        return new EngineImpl(connection);
+    }
+
+    @Bean
+    PersonDAO personDAO(Connection connection) {
+        return new PersonImpl(connection);
+    }
+
+
+}

f3d4c330892816fe9d5e3f766c2eb6cdb849726a
stritron <finalroot@gmail.com>
2019-03-01 00:52:19 +0300

    Moved cabinet to 'service'

--- src/main/java/ru/inno/service/CabinetService.java
+++ src/main/java/ru/inno/service/CabinetService.java
@@ -1,4 +1,4 @@
-package ru.inno.services;
+package ru.inno.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;

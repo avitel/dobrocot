ea3c909580282a5ef327955b8e235bb3294db54e
Ilia Balakirev <i.balakirev.stc@innopolis.university>
2019-03-03 22:58:51 +0300

    Add CarReservationService tests, add and realised one method in CarReservableService

--- src/main/java/ru/inno/service/ReservableService.java
+++ src/main/java/ru/inno/service/ReservableService.java
@@ -10,5 +10,6 @@ public interface ReservableService {
     Map getReservedDates(String car_id);
     int getDays(Date d1, Date d2) throws Exception;
     boolean checkAvailableDate(Date d1, Date d2, String car_id);
+    boolean checkAvailableCustomer(int id_seller);
     void addReservationOrder(int id_car, int id_owner, /*int id_customer,*/ Date date_begin, Date date_end, int price);
 }

249ceb90de1db2b86bd2a8de842c17a5b3d3a584
MironKim <mithoughts@yandex.ru>
2019-03-04 08:54:59 +0300

    Тест контроллера и сервиса страницы личного кабинета.

---
+++ src/test/java/ru/inno/controller/CabinetControllerTest.java
@@ -0,0 +1,73 @@
+package ru.inno.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import ru.inno.entity.Car;
+import ru.inno.entity.Order;
+import ru.inno.entity.Person;
+import ru.inno.service.CabinetService;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+class CabinetControllerTest {
+
+    @Mock
+    private CabinetService cabinetService;
+    @InjectMocks
+    private CabinetController cabinetController;
+    private List<Order> orders;
+    private MockMvc mockMvc;
+    private List<Car> cars;
+    private Person person;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(cabinetController).build();
+
+        person = new Person();
+        cars = Collections.singletonList(new Car());
+        orders = Collections.singletonList(new Order());
+    }
+
+    @Test
+    void cabinetGET() throws Exception {
+        when(cabinetService.getCurrentPerson()).thenReturn(person);
+        when(cabinetService.getCarList(any(Person.class))).thenReturn(cars);
+        when(cabinetService.getSellerOrders(any(Person.class))).thenReturn(orders);
+        when(cabinetService.getCustomerOrders(any(Person.class))).thenReturn(orders);
+
+        mockMvc.perform(get("/cabinet/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("cabinet"))
+                .andExpect(model().hasNoErrors())
+                .andExpect(model().attributeExists("carList", "customerOrders", "sellerOrders", "currentPerson"))
+                .andExpect(model().attribute("carList", cars))
+                .andExpect(model().attribute("customerOrders", orders))
+                .andExpect(model().attribute("sellerOrders", orders))
+                .andExpect(model().attribute("currentPerson", person)
+                );
+    }
+
+    @Test
+    void cabinetPOST() throws Exception {
+        mockMvc.perform(post("/cabinet/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(model().hasNoErrors());
+    }
+}
